5f0a238d0ffd08fa8822683f0657dfaf1c5e849b
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2018-05-11 10:55:34 +0200

    Updated

--- senia-kiosko.install/etc/profile.d/senia-kiosko-browser.sh
+++ senia-kiosko.install/etc/profile.d/senia-kiosko-browser.sh
@@ -4,5 +4,9 @@
 URLKIOSKO="http://forms.edu.gva.es/limesurvey/index.php/823645"
 
 logger -t " Senia Kiosko " " Starting FirefoX  : $URLKIOSKO"
-firefox -private "${URLKIOSKO}"
+firefox -private "${URLKIOSKO}" & 
+
+sleep 1
+xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11
+
 
