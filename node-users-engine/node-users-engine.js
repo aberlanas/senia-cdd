964e3c5e77f093a11099bfd90aa92880bdf752fd
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2018-08-05 18:04:24 +0200

    Updated with alumnes  option

--- node-users-engine/node-users-engine.js
+++ node-users-engine/node-users-engine.js
@@ -6,24 +6,28 @@ var fs = require('fs');
 var parser = require('xml2json');
 
 function info (filename) {
-    fs.readFile(filename.filename, function(err,data){
+    fs.readFile(filename, function(err,data){
       var centre = parser.toJson(data);
       var JCentre = JSON.parse(centre);
       console.log(JCentre);
     });
 }
 
-function alumnes = (filename) => {
-  console.log("Patata");
+function alumnes (filename) {
+  fs.readFile(filename, function(err,data){
+    var centre = parser.toJson(data);
+    var JCentre = JSON.parse(centre);
+    console.log(JCentre.alumnes);
+  });
 }
 
 var program = require('commander');
 
 program
   .version('0.1')
-  .option('-i, --info [filename]',"Engine CLI for Users and Groups")
-  .option('-a, --alumnes <filename>',"Show Alumnes")
+  .option('-i, --info [filename]',"Engine CLI for Users and Groups","llxgesc.dat")
+  .option('-a, --alumnes [filename]',"Show Alumnes")
   .parse(process.argv);
 
-
-if (program.info) info(program.filename);
+if (program.info) info(program.info);
+if (program.alumnes) alumnes(program.alumnes);
