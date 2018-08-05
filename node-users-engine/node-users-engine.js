21e5fa1edfa509c67090410babc87b587e3e33a9
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2018-08-05 18:11:20 +0200

    Some Updates

--- node-users-engine/node-users-engine.js
+++ node-users-engine/node-users-engine.js
@@ -17,7 +17,7 @@ function alumnes (filename) {
   fs.readFile(filename, function(err,data){
     var centre = parser.toJson(data);
     var JCentre = JSON.parse(centre);
-    console.log(JCentre.alumnes);
+    console.log(JCentre.centre.alumnes);
   });
 }
 
@@ -26,8 +26,10 @@ var program = require('commander');
 program
   .version('0.1')
   .option('-i, --info [filename]',"Engine CLI for Users and Groups","llxgesc.dat")
-  .option('-a, --alumnes [filename]',"Show Alumnes")
+  .option('-a, --alumnes [filename]',"Show Alumnes","llxgesc.dat")
   .parse(process.argv);
+
+
 
 if (program.info) info(program.info);
 if (program.alumnes) alumnes(program.alumnes);
