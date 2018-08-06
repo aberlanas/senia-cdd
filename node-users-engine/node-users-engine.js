64622f6a3ab3d239250986f002d046426a196f4e
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2018-08-06 09:52:30 +0200

    Updated with Sync

--- node-users-engine/node-users-engine.js
+++ node-users-engine/node-users-engine.js
@@ -6,30 +6,53 @@ var fs = require('fs');
 var parser = require('xml2json');
 
 function info (filename) {
-    fs.readFile(filename, function(err,data){
-      var centre = parser.toJson(data);
-      var JCentre = JSON.parse(centre);
-      console.log(JCentre);
-    });
+    var data = fs.readFileSync(filename);
+    var centre = parser.toJson(data);
+    var JCentre = JSON.parse(centre);
+    console.log(JCentre);
+    
 }
 
 function alumnes (filename) {
-  fs.readFile(filename, function(err,data){
+  
+    var data = fs.readFileSync(filename);
     var centre = parser.toJson(data);
     var JCentre = JSON.parse(centre);
-    console.log(JCentre.centre.alumnes);
-  });
+    JCentre.centre.alumnes.forEach(alumne => {
+      console.log(alumne.nom);  
+    });
+    
+}
+  
 }
 
 var program = require('commander');
 
+
 program
   .version('0.1')
-  .option('-i, --info [filename]',"Engine CLI for Users and Groups","llxgesc.dat")
-  .option('-a, --alumnes [filename]',"Show Alumnes","llxgesc.dat")
+  .option('-f, --file [filename]',"File of Users and Groups")
+  .option('-i, --info',"Engine CLI for Users and Groups")
+  .option('-a, --alumnes',"Show Alumnes")
   .parse(process.argv);
 
 
+  var filename = "";
+  
+  if(program.file){
+    filename = program.file;
+  }
 
-if (program.info) info(program.info);
-if (program.alumnes) alumnes(program.alumnes);
+if (program.info){
+
+  info(filename);
+  process.exit(0);
+
+}
+
+if (program.alumnes) {
+
+  alumnes(filename);
+  process.exit(0);
+
+}
