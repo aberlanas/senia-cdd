9c5132236d2e41549cfa22a3a7c6d06a1af3ffce
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2018-09-06 06:40:20 +0200

    Updated

--- senia-browsers-settings.install/etc/firefox/syspref.senia.js
+++ senia-browsers-settings.install/etc/firefox/syspref.senia.js
@@ -1,2 +1,14 @@
 // Senia settings
-lockPref("browser.startup.homepage", "https://docent.edu.gva.es/md-front/www/ | http://ieslasenia.org | http://revista.ieslasenia.org/ |  https://moodle.ieslasenia.org/ ");
+lockPref("browser.startup.homepage", "https://docent.edu.gva.es/md-front/www/ | http://ieslasenia.org | http://revista.ieslasenia.org/ |  https://moodle.ieslasenia.org/ | 172.29.0.245 ");
+
+
+lockPref("pref.privacy.disable_button.view_passwords", true); //Disable the "Show Passwords" Button
+lockPref("privacy.donottrackheader.enabled", true); //no targeted ads
+lockPref("privacy.item.formdata", true); //clear saved form data
+lockPref("privacy.item.offlineApps", true); //clear offline apps data
+lockPref("security.enable_ssl3", true); //Use SSL 3.0
+lockPref("security.enable_tls", true); //Use TLS 1.0
+
+
+// Settings for PDFs
+lockPref("browser.helperApps.neverAsk.openFile",false);
