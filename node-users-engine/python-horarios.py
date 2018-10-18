acdff2d50e2f9c165fe4120a04067374dea0ce2e
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2018-10-18 08:49:01 +0200

    Updated

---
+++ node-users-engine/python-horarios.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+
+import os
+import sys
+from xml.etree import ElementTree
+
+fileDb="/home/aberlanas/Descargas/imexalum.xml"
+
+
+def sanity_checks():
+    if (os.path.exists(fileDb)):
+        print(" * File Exists!")
+    else:
+        print(" * Dame un ficherin ")
+        sys.exit(1)
+
+
+def read_all(f):
+    with open(f,'rt') as f:
+        tree = ElementTree.parse(f)
+
+    for prof in tree.iter('docente');
+    for node in tree.iter('horario_ocupacion'):
+        print(node.attrib)
+        print(node.attrib['plantilla'])
+
+
+if __name__ == "__main__":
+
+    sanity_checks()
+    read_all(fileDb)
+
+
+
+sys.exit(0)
+
