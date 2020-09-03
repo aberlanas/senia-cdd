fc97118e9edcb29cda2ac917b6e494d38672fc9a
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2020-09-03 07:53:21 +0200

    Creating LOPD Compliant forms

---
+++ tools/python-list-users-lopd.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+ # -*- coding: utf-8 -*-
+
+from openpyxl import Workbook
+from openpyxl import load_workbook
+
+listFile = "/home/aberlanas/Descargas/1ESO_2021.xlsx"
+
+
+wb = load_workbook(listFile)
+
+for sheet_name in wb.sheetnames:
+
+   sheet = wb[sheet_name]
+   print( "" )
+   print( "# Grup : " + sheet['A1'].value)
+   print( "")
+
+   print("|Nº| Nom |Cognoms|")
+   print("|:-:|:---:|:------|")
+   
+   numA = 1
+   for row in sheet.iter_rows(min_row=4,max_row=24):
+       nombre = None
+
+       try:
+           nombre = row[0].value
+       except Exception as e:
+           print(str(e))
+           pass
+
+       if nombre != None:
+           #print (str(row[0].value))
+           nombrel = nombre.split(',')
+
+           nom = nombrel[1]
+           ape = nombrel[0]
+
+           nomFinal = ""
+           noml = nom.strip().split(' ')
+           for auxn in noml:
+               nomFinal += auxn.capitalize() + " "
+
+           apelFinal = ""    
+           apel = ape.strip().split(' ')
+
+           for auxp in apel:
+               auxf = auxp[0:2]
+
+               sasteriscos = ""
+               nasteriscos = len(auxp)-2
+               for i in range(nasteriscos):
+                   sasteriscos += "*"
+
+               apelFinal+=auxf.capitalize()+sasteriscos+" "
+
+           print ("| " + str(numA) + " | "+ nomFinal + "|" + apelFinal +"|")
+           numA = numA + 1
+
+   print(" ")
+   print("---")
+   print("\\newpage")
+
+
